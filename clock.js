4bed56abead7da7624ddbb681ebe21d879a8caf8
rahul0597 <ra.rathodb@gmail.com>
2021-07-07 00:57:00 +0530

    Initial Commit

---
+++ clock.js
@@ -0,0 +1,15 @@
+let a=new Date();
+let date=a.getDate();
+let month=a.getMonth();
+let year=a.getFullYear();
+
+let time=a.getHours()+':'+a.getMinutes();
+
+document.getElementById("time").innerHTML="hhhh"
+document.getElementById("para").innerHTML="hi";
+
+
+function browseMore()
+{
+	console.log("browsed")
+}
